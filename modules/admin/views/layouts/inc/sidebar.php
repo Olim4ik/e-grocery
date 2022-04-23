96c90ee7f4f6b599ed3e559cd46d575925e159b0
Olim4ik <64145001+Olim4ik@users.noreply.github.com>
2022-04-23 15:57:15 +0500

    Remove user info in sidebar

--- modules/admin/views/layouts/inc/sidebar.php
+++ modules/admin/views/layouts/inc/sidebar.php
@@ -4,21 +4,21 @@
 	<!-- sidebar: style can be found in sidebar.less -->
 	<section class="sidebar">
 
-		<!-- Sidebar user panel (optional) -->
-		<div class="user-panel">
-			<div class="pull-left image">
-				<img src="dist/img/user2-160x160.jpg" class="img-circle" alt="User Image">
-			</div>
-			<div class="pull-left info">
-				<?= Yii::$app->user->identity->username ?>
-				<a href="#"><i class="fa fa-circle text-success"></i> Online</a>
-			</div>
-		</div>
+<!--		 Sidebar user panel (optional) -->
+<!--		<div class="user-panel">-->
+<!--			<div class="pull-left image">-->
+<!--				<img src="dist/img/user2-160x160.jpg" class="img-circle" alt="User Image">-->
+<!--			</div>-->
+<!--			<div class="pull-left info">-->
+<!--				--><?//= Yii::$app->user->identity->username ?>
+<!--				<a href="#"><i class="fa fa-circle text-success"></i> Online</a>-->
+<!--			</div>-->
+<!--		</div>-->
 
 
 		<!-- Sidebar Menu -->
 		<ul class="sidebar-menu" data-widget="tree">
-			<li class="header">HEADER</li>
+			<li class="header"><b>Grocery . Admin . Panel</b></li>
 			<!-- Optionally, you can add icons to the links -->
 			<li class="active"><a href="<?= \yii\helpers\Url::to(['main/index']) ?>"><i class="fa fa-bar-chart"></i> <span>Статистика магазина</span></a></li>
 			<li><a href="#"><i class="fa fa-link"></i> <span>Another Link</span></a></li>
