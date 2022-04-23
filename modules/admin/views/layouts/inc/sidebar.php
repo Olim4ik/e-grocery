ec8b2634f1afb5c4832aff0dab3fb98524f08b5b
Olim4ik <64145001+Olim4ik@users.noreply.github.com>
2022-04-23 15:59:31 +0500

    small edits in sidebar.php

--- modules/admin/views/layouts/inc/sidebar.php
+++ modules/admin/views/layouts/inc/sidebar.php
@@ -1,25 +1,7 @@
-<!-- Left side column. contains the logo and sidebar -->
 <aside class="main-sidebar">
-
-	<!-- sidebar: style can be found in sidebar.less -->
 	<section class="sidebar">
-
-<!--		 Sidebar user panel (optional) -->
-<!--		<div class="user-panel">-->
-<!--			<div class="pull-left image">-->
-<!--				<img src="dist/img/user2-160x160.jpg" class="img-circle" alt="User Image">-->
-<!--			</div>-->
-<!--			<div class="pull-left info">-->
-<!--				--><?//= Yii::$app->user->identity->username ?>
-<!--				<a href="#"><i class="fa fa-circle text-success"></i> Online</a>-->
-<!--			</div>-->
-<!--		</div>-->
-
-
-		<!-- Sidebar Menu -->
 		<ul class="sidebar-menu" data-widget="tree">
 			<li class="header"><b>Grocery . Admin . Panel</b></li>
-			<!-- Optionally, you can add icons to the links -->
 			<li class="active"><a href="<?= \yii\helpers\Url::to(['main/index']) ?>"><i class="fa fa-bar-chart"></i> <span>Статистика магазина</span></a></li>
 			<li><a href="#"><i class="fa fa-link"></i> <span>Another Link</span></a></li>
 			<li class="treeview">
@@ -34,7 +16,5 @@
 				</ul>
 			</li>
 		</ul>
-		<!-- /.sidebar-menu -->
 	</section>
-	<!-- /.sidebar -->
 </aside>
